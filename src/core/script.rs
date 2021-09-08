6efb856bc9fc794a48b9aee83aa01ce1a074c3cf
Mr-Leshiy <leshiy12345678@gmail.com>
2021-09-08 20:12:44 +0300

    refactor Script struct

--- src/core/script.rs
+++ src/core/script.rs
@@ -5,6 +5,26 @@ struct Script {
     data: Vec<u8>,
 }
 
+impl Script {
+    // FIXME remove #[allow(dead_code)]
+    #[allow(dead_code)]
+    fn new() -> Self {
+        Self { data: Vec::new() }
+    }
+
+    // FIXME remove #[allow(dead_code)]
+    #[allow(dead_code)]
+    fn push_argument(&mut self, arg: &Argument) {
+        self.data.append(&mut arg.to_script());
+    }
+
+    // FIXME remove #[allow(dead_code)]
+    #[allow(dead_code)]
+    fn push_op_code(&mut self, op_code: &OpCode) {
+        self.data.append(&mut op_code.encode());
+    }
+}
+
 #[derive(Debug)]
 enum ScriptError {
     UnknownOpCode(OpCode),
@@ -133,169 +153,122 @@ mod tests {
 
     #[test]
     fn op_add_test() {
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_ADD.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_op_code(&OP_ADD);
 
         assert_eq!(
-            u64::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            u64::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(11)
         );
     }
 
     #[test]
     fn op_sub_test() {
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_SUB.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_op_code(&OP_SUB);
 
         assert_eq!(
-            u64::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            u64::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(1)
         );
     }
 
     #[test]
     fn op_eql_test() {
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_EQL.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_op_code(&OP_EQL);
 
         assert_eq!(
-            bool::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            bool::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(true)
         );
 
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_EQL.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_op_code(&OP_EQL);
 
         assert_eq!(
-            bool::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            bool::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(false)
         );
     }
 
     #[test]
     fn op_nql_test() {
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_NQL.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_op_code(&OP_NQL);
 
         assert_eq!(
-            bool::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            bool::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(true)
         );
 
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (5 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_NQL.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (5 as u64).encode(),
+        });
+        script.push_op_code(&OP_NQL);
 
         assert_eq!(
-            bool::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            bool::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(false)
         );
     }
 
     #[test]
     fn script_test() {
-        let mut data = Vec::new();
-        data.append(
-            &mut Argument {
-                data: (6 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(
-            &mut Argument {
-                data: (8 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_ADD.encode());
-        data.append(
-            &mut Argument {
-                data: (12 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_SUB.encode());
-        data.append(
-            &mut Argument {
-                data: (2 as u64).encode(),
-            }
-            .to_script(),
-        );
-        data.append(&mut OP_EQL.encode());
+        let mut script = Script::new();
+        script.push_argument(&Argument {
+            data: (6 as u64).encode(),
+        });
+        script.push_argument(&Argument {
+            data: (8 as u64).encode(),
+        });
+        script.push_op_code(&OP_ADD);
+        script.push_argument(&Argument {
+            data: (12 as u64).encode(),
+        });
+        script.push_op_code(&OP_SUB);
+        script.push_argument(&Argument {
+            data: (2 as u64).encode(),
+        });
+        script.push_op_code(&OP_EQL);
 
         assert_eq!(
-            bool::decode(&mut Script { data }.evaluate().unwrap().unwrap().data.as_ref()),
+            bool::decode(&mut script.evaluate().unwrap().unwrap().data.as_ref()),
             Ok(true)
         );
     }
