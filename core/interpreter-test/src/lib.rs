6bb372612b352fad6eeb21d10a875dc987753fe0
Alex Pozhylenkov <leshiy12345678@gmail.com>
2021-09-29 14:41:37 +0300

    add OpCodeVal trait (#21)

--- core/interpreter-test/src/lib.rs
+++ core/interpreter-test/src/lib.rs
@@ -1,5 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use interpreter::eval;
+    use script::{opcode::OpAdd, tests::default_script};
+
     #[test]
-    fn eval_test() {}
+    fn eval_test() {
+        let script = default_script();
+        eval!(script, OpAdd {});
+    }
 }
--- core/script/src/opcode.rs
+++ core/script/src/opcode.rs
@@ -1,12 +1,86 @@
 use crate::argument::Argument;
+use impl_trait_for_tuples::impl_for_tuples;
 use parity_scale_codec::{Decode, Encode};
 
+#[derive(Debug)]
+pub enum OpCodeError {
+    InvalidArgumentAmount,
+    UnexepectedArgumentType,
+}
+
 pub trait OpCode {
-    type Args: Encode + Decode;
-    type Res: Encode + Decode;
+    type Args: OpCodeVal;
+    type Res: OpCodeVal;
     const CODE: u32;
 
     fn handler(args: Self::Args) -> Self::Res;
+}
+
+pub trait OpCodeVal: Sized + Encode + Decode {
+    fn decode_arguments(args_stack: &mut Vec<Argument>) -> Result<Self, OpCodeError> {
+        args_stack
+            .pop()
+            .ok_or(OpCodeError::InvalidArgumentAmount)?
+            .get_value()
+            .map_err(|_| OpCodeError::UnexepectedArgumentType)
+    }
+
+    fn encode_arguments(self, args_stack: &mut Vec<Argument>) {
+        args_stack.push(Argument::new().set_value_chain(self));
+    }
+}
+
+impl OpCodeVal for u8 {}
+impl OpCodeVal for u16 {}
+impl OpCodeVal for u32 {}
+impl OpCodeVal for u64 {}
+impl OpCodeVal for u128 {}
+
+impl OpCodeVal for i8 {}
+impl OpCodeVal for i16 {}
+impl OpCodeVal for i32 {}
+impl OpCodeVal for i64 {}
+impl OpCodeVal for i128 {}
+
+impl OpCodeVal for bool {}
+
+impl OpCodeVal for Argument {
+    fn decode_arguments(args_stack: &mut Vec<Argument>) -> Result<Self, OpCodeError> {
+        args_stack.pop().ok_or(OpCodeError::InvalidArgumentAmount)
+    }
+
+    fn encode_arguments(self, args_stack: &mut Vec<Argument>) {
+        args_stack.push(self);
+    }
+}
+
+#[impl_for_tuples(15)]
+impl OpCodeVal for Tuple {
+    fn decode_arguments(args_stack: &mut Vec<Argument>) -> Result<Self, OpCodeError> {
+        let res = for_tuples!( ( #( Tuple::decode_arguments(args_stack)? ), *) );
+        Ok(res)
+    }
+
+    fn encode_arguments(self, args_stack: &mut Vec<Argument>) {
+        for_tuples!( #( Tuple.encode_arguments(args_stack); )* );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_arguments_test() {
+        let mut args = Vec::new();
+
+        args.push(Argument::new().set_value_chain(5_u64));
+        args.push(Argument::new().set_value_chain(11_u64));
+
+        let (val1, val2) = <(u64, u64)>::decode_arguments(&mut args).unwrap();
+        assert_eq!(val1, 11_u64);
+        assert_eq!(val2, 5_u64);
+    }
 }
 
 pub struct OpPush;
