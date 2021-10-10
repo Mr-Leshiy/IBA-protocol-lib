5bc21ca3800d1da7fcffdb5f617dcb6b0deaf2a8
Alex Pozhylenkov <leshiy12345678@gmail.com>
2021-10-10 11:20:11 +0300

    Refactor interpreter (#23)

--- core/script/src/lib.rs
+++ core/script/src/lib.rs
@@ -1,5 +1,5 @@
 use opcode::*;
-use parity_scale_codec::{Decode, Encode, Error};
+use parity_scale_codec::{Decode, Encode, Error, Input};
 
 pub mod opcode;
 
@@ -37,11 +37,26 @@ impl ScriptValue {
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Default, Debug)]
 pub struct Script {
     data: Vec<u8>,
+
+    #[codec(skip)]
+    index: usize,
+}
+
+#[derive(Debug)]
+pub enum ScriptError {
+    CannotDecodeOpCode,
+    CannotDecodeValue,
+    EndOfScript,
+    UnknownOpCode(u32),
+    InvalidArguments(OpCodeError),
 }
 
 impl Script {
     pub fn new() -> Self {
-        Self { data: Vec::new() }
+        Self {
+            data: Vec::new(),
+            index: 0,
+        }
     }
 
     pub fn push_value<T: Encode>(&mut self, val: &T) {
@@ -68,16 +83,41 @@ impl Script {
         self
     }
 
-    // TODO: remove this, implement parity_scale_codec::Input trait
-    pub fn get_data(&self) -> Vec<u8> {
-        self.data.clone()
+    pub fn try_next_opcode(&mut self) -> Result<Option<u32>, ScriptError> {
+        if self.data.len() - self.index != 0 {
+            Ok(Some(
+                u32::decode(self).map_err(|_| ScriptError::CannotDecodeOpCode)?,
+            ))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn try_next_value(&mut self) -> Result<Option<ScriptValue>, ScriptError> {
+        if self.data.len() - self.index != 0 {
+            Ok(Some(
+                ScriptValue::decode(self).map_err(|_| ScriptError::CannotDecodeValue)?,
+            ))
+        } else {
+            Ok(None)
+        }
     }
 }
 
-#[derive(Debug)]
-pub enum ScriptError {
-    UnknownOpCode(u32),
-    InvalidArguments(OpCodeError),
+impl Input for Script {
+    fn remaining_len(&mut self) -> Result<Option<usize>, Error> {
+        Ok(Some(self.data.len() - self.index))
+    }
+
+    fn read(&mut self, into: &mut [u8]) -> Result<(), Error> {
+        if into.len() > self.data.len() - self.index {
+            return Err("Not enough data to fill buffer".into());
+        }
+        let len = into.len();
+        into.copy_from_slice(&self.data[self.index..self.index + len]);
+        self.index += len;
+        Ok(())
+    }
 }
 
 pub mod tests {
